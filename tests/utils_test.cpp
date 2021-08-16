937d54283759bbf364be468579964edfbafbc226
Alper OZKAN <alper.ozkan.p@gmail.com>
2021-08-16 22:56:46 +0200

    Test write to file done

--- tests/utils_test.cpp
+++ tests/utils_test.cpp
@@ -27,3 +27,10 @@ TEST_F(UtilsTest, readFromFile2) {
 	std::string s = famTree::readFromFile(fs::path(PROJECT_PATH) /= "tests/readFromFileTest.txt");
 	EXPECT_NE(s, "Test reading file unequal");
 }
+
+TEST_F(UtilsTest, writeToFile1) {
+	std::string messageToWrite = "Test write to file";
+	fs::path pathToTestFile = fs::path(PROJECT_PATH) /= "tests/writeToFile.txt";
+	famTree::writeToFile(messageToWrite, pathToTestFile);
+	EXPECT_EQ(messageToWrite, famTree::readFromFile(pathToTestFile));
+}
